f4f6d40a31696d2d0a158d15a44d7847653b8846
hoyawolfer <hoyawolfer@163.com>
2017-10-10 15:04:28 +0800

    api

---
+++ V2exDemo/Model/Topic.swift
@@ -0,0 +1,9 @@
+//
+//  Topic.swift
+//  V2exDemo
+//
+//  Created by zaixuan on 2017/10/10.
+//  Copyright © 2017年 Zaixuan. All rights reserved.
+//
+
+import Foundation
---
+++ V2exDemo/Tools/HTMLParser.swift
@@ -0,0 +1,25 @@
+//
+//  HTMLParser.swift
+//  V2exDemo
+//
+//  Created by zaixuan on 2017/10/10.
+//  Copyright © 2017年 Zaixuan. All rights reserved.
+//
+
+import Foundation
+import Ji
+
+struct HTMLParser {
+    static let shared = HTMLParser()
+
+    private init() {
+
+    }
+
+    func HomeModel(data:Data) -> <#return type#> {
+        <#function body#>
+    }
+
+
+}
+
